64e05e2e5cdde4643ac19bf0acf99ecb5388218b
cwtablante <carlwilliam.tablante@gmail.com>
2021-11-10 10:39:05 +0800

    Expose /issues endpoint

--- main.go
+++ main.go
@@ -1,16 +1,58 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
 
 	"github.com/carlqt/jira-ya/jira"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func main() {
-	issues, err := jira.GetIssues()
-	if err != nil {
-		panic(err)
-	}
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-	fmt.Println(issues)
+	router := mux.NewRouter()
+	router.Handle("/issues", GetIssues()).Methods("GET")
+	router.Use(ResponseHeaderHandler)
+
+	corsOptions := cors.New(cors.Options{
+		// AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{"OPTIONS", "GET", "POST"},
+		AllowedHeaders: []string{"Content-Type"},
+	})
+
+	log.Println("starting at port 8000")
+	loggedRouter := handlers.LoggingHandler(os.Stdout, router)
+	http.ListenAndServe(":8000", corsOptions.Handler(loggedRouter))
+
+}
+
+func GetIssues() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		issues, err := jira.GetIssues()
+		// Create new game
+		// when successful, move state to "waiting for players"
+		// go routine to reply
+		// go routine for a countdown timer and move to next state
+		if err != nil {
+			w.WriteHeader(http.StatusUnprocessableEntity)
+			w.Write([]byte(err.Error()))
+		} else {
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(issues)
+		}
+
+	})
+}
+
+func ResponseHeaderHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		next.ServeHTTP(w, r)
+	})
 }
