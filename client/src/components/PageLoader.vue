6126c05aafc109b78f37d11263e080a63e6cc1e2
cwtablante <carlwilliam.tablante@gmail.com>
2021-11-12 08:01:48 +0800

    Added PageLoaded component

---
+++ client/src/components/PageLoader.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="container">
+      <div class="col-sm-3">
+          <div class="panel panel-default">
+              <div class="panel-heading clearfix">
+                  <div class="col-xs-10">
+                      <div class="animate-bg"></div>
+                  </div>
+              </div>
+              <div class="panel-body">
+                  <div class="col-sm-5">
+                      <p class="animate-bg"><br><br><br></p>
+                  </div>
+                  <div class="col-sm-7">
+                      <p class="animate-bg"></p>
+                      <p class="animate-bg col-sm-11"></p>
+                      <p class="animate-bg"></p>
+                  </div>
+              </div>
+          </div>
+          <div class="panel panel-default">
+              <div class="panel-heading clearfix">
+                  <div class="col-xs-10">
+                      <div class="animate-bg"></div>
+                  </div>
+              </div>
+              <div class="panel-body">
+                  <div class="col-sm-5">
+                      <p class="animate-bg"><br><br><br></p>
+                  </div>
+                  <div class="col-sm-7">
+                      <p class="animate-bg"></p>
+                      <p class="animate-bg col-sm-11"></p>
+                      <p class="animate-bg"></p>
+                  </div>
+              </div>
+          </div>
+      </div>
+     
+      <div class="col-sm-9">
+          <div class="panel panel-default">
+              <div class="panel-heading clearfix">
+                  <div class="col-xs-10">
+                      <div class="animate-bg"></div>
+                  </div>
+              </div>
+              <div class="panel-body">
+                  <div class="clearfix">
+                      <div class="col-sm-3">
+                          <p class="animate-bg"><br><br><br><br><br><br></p>
+                      </div>
+                      <div class="col-sm-9">
+                          <p class="animate-bg col-sm-11"></p>
+                          <p class="animate-bg col-sm-9"></p>
+                          <p class="animate-bg col-sm-10"></p>
+                          <p class="animate-bg col-sm-9"></p>
+                          <p class="animate-bg"></p>
+                      </div>
+                  </div>
+
+                  <br>
+
+                  <div class="col-sm-6">
+                      <p class="animate-bg col-sm-11"></p>
+                      <p class="animate-bg col-sm-9"></p>
+                      <p class="animate-bg col-sm-10"></p>
+                      <p class="animate-bg col-sm-9"></p>
+                      <p class="animate-bg"></p>
+                  </div>
+                  <div class="col-sm-6">
+                      <p class="animate-bg"><br><br><br><br><br><br></p>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageLoader'
+}
+</script>
+
+<style>
+@keyframes placeHolderShimmer {
+	0% {
+		background-position: -400px 0
+	}
+	100% {
+		background-position: 400px 0
+	}
+}
+
+.animate-bg {
+	animation-duration: 1s;
+	animation-fill-mode: forwards;
+	animation-iteration-count: infinite;
+	animation-name: placeHolderShimmer;
+	animation-timing-function: linear;
+	background: #f6f7f8;
+	background: linear-gradient(to right, #eeeeee 8%, #dddddd 18%, #eeeeee 33%);
+	background-size: 800px 104px;
+	min-height: 12px;
+	display: block;
+	clear: both;
+}
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PageLoader v-if='loading'/>
+    <table class="primary" v-else>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>City</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Francisco</td>
+          <td>Valencia, Spain</td>
+        </tr>
+        <tr>
+          <td>Eve</td>
+          <td>San Francisco, USA</td>
+        </tr>
+        <tr>
+          <td>Martin</td>
+          <td>New York, USA</td>
+        </tr>
+        <tr>
+          <td>Sarah</td>
+          <td>London, UK</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PageLoader from '@/components/PageLoader.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    PageLoader,
+  },
+  data () {
+    return {
+      loading: true,
+    }
+  },
+  async mounted () {
+    try {
+      const resp = await fetch('http://localhost:8000/issues')
+      const jsonResp = await resp.json()
+      this.loading = false
+      console.log(jsonResp)
+    } catch (e) {
+      console.error(e)
+    }
+  },
 }
 </script>
